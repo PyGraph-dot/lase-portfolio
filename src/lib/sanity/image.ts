235007016f4a2212011d18b8ba5339a99c05604b
PyGraph-dot <techpolyglotpro@gmail.com>
2026-01-15 04:00:40 +0100

    Fix Tailwind CSS import and add missing dependency

--- src/lib/sanity/image.ts
+++ src/lib/sanity/image.ts
@@ -1,7 +1,7 @@
-import { createImageUrlBuilder } from '@sanity/image-url'
+import imageUrlBuilder from '@sanity/image-url'
 import { client } from './client'
 
-const imageBuilder = createImageUrlBuilder(client)
+const imageBuilder = imageUrlBuilder(client)
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const urlFor = (source: { asset: { _ref: string } }) => {
